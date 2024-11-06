0109032b835b27f9f7c7f3e2289918a091905f18
𝕯𝖊𝖒𝖔𝖓 <78769380+codegod100@users.noreply.github.com>
2024-11-05 20:31:31 -0800

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<div class="column has-background-dark is-1">
+  <div>Servers</div>
+  <div>
+    <img alt="test" class="image is-64x64 is-rounded" src="/duck.png" />
+  </div>
+</div>
+<div class="column is-3">
+  <div>Channels</div>
+  <div>#yolo</div>
+</div>
+<div class="column">
+  <div>Chat</div>
+  <div>What up chat?</div>
+
+  <div><input class="input" type="text" /></div>
+</div>
